0fb2497dfdd43e493b697af5f7309ad3d29c8d79
wisnu912 <mrwisnuhadi88@gmail.com>
2025-10-29 14:44:44 +0700

    update blade to use manifest in production

--- resources/views/Auth/auth.blade.php
+++ resources/views/Auth/auth.blade.php
@@ -5,7 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
-    @vite('resources/css/app.css')
+  @if (app()->environment('local'))
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+@else
+    @php
+        $manifest = json_decode(file_get_contents(public_path('build/manifest.json')), true);
+    @endphp
+    <link rel="stylesheet" href="{{ asset('build/'.$manifest['resources/css/app.css']['file']) }}">
+    <script type="module" src="{{ asset('build/'.$manifest['resources/js/app.js']['file']) }}"></script>
+@endif
+
 </head>
 <body class="bg-gradient-to-r from-white  to-sky-200">
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-      @vite('resources/css/app.css')
+    @if (app()->environment('local'))
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+@else
+    @php
+        $manifest = json_decode(file_get_contents(public_path('build/manifest.json')), true);
+    @endphp
+    <link rel="stylesheet" href="{{ asset('build/'.$manifest['resources/css/app.css']['file']) }}">
+    <script type="module" src="{{ asset('build/'.$manifest['resources/js/app.js']['file']) }}"></script>
+@endif
+
 </head>
 <body class="bg-gray-200 ">
     <h1 class="text-base font-bold text-center mt-20 lg:text-5xl">
